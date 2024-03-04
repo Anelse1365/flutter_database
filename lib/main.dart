ccc566c04437dc1a2433a08ab7c7ea0710774444
Anelse1365 <130572929+Anelse1365@users.noreply.github.com>
2024-03-04 19:47:43 +0700

    uwhooooa

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => TransactionProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'My Account',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: ''),
-        routes: {
-          '/test2': (context) => const HeroListPage(),
-          '/heroListPage': (context) => const HeroListPage(),
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Account',
+      home: const MyHomePage(title: ''),
+      routes: {
+        '/test2': (context) => const HeroListPage(),
+        '/heroListPage': (context) => const HeroListPage(),
+      },
     );
   }
 }
@@ -42,65 +32,90 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TransactionProvider provider =
-        Provider.of<TransactionProvider>(context, listen: false);
+    var size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/test2');
-            },
-            icon: const Text(
-              "ดูแคลอรี่อาหาร",
-              style: TextStyle(fontSize: 14.0),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.35,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 120, 194, 255),
+            ),
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0, bottom: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Check Food Calories",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    "อาหารแคลเยอะยอดฮิต",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 360,
+            left: 0,
+            child: ImageSlider(),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 600.0),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/test2');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 156, 141, 255),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                ),
+                child: Text(
+                  "ดูแคลอรี่อาหาร",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'อาหารแคลสูงยอดฮิต',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-              child: Image(
-            image: AssetImage('assets/dessert/2.jpg'),
-          )),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Total Calories:',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '${provider.totalCalories}',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.deepPurple,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+    );
+  }
+}
+
+class ImageSlider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150, // ปรับความสูงของ Slide ตามต้องการ
+      width: MediaQuery.of(context).size.width,
+      child: PageView.builder(
+        itemCount: 3, // จำนวนรูปภาพ
+        itemBuilder: (context, index) {
+          return Image.asset(
+            'assets/album/${index + 1}.jpg',
+            fit: BoxFit.cover,
+          );
+        },
       ),
     );
   }
